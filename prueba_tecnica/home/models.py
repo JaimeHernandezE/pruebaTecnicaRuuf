da8870b94581f5de33f46647aa4caf96fc435da4
Jaime Hernandez <jhearquitecto@gmail.com>
2024-12-13 14:20:46 -0300

    Genero el modelo para almacenar los datos de Cabida

--- prueba_tecnica/home/models.py
+++ prueba_tecnica/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cabida(models.Model):
+    lado_a = models.FloatField()
+    lado_b = models.FloatField()
+    lado_x = models.FloatField()
+    lado_y = models.FloatField()
+    resultado = models.IntegerField(null=True, blank=True)
